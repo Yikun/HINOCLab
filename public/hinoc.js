3ae65db91bba82048eca22663dd2a5e390a49a98
Jiang Yikun <yikunkero@gmail.com>
2014-05-03 10:43:35 +0800

    Init

---
+++ public/hinoc.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#submit").click(function(){
+  	console.log("task:"+
+  				$("#grade").val() +
+  				$("#name").val() +
+  				$("#time").val() +
+  				$("#task").val() +
+  				$("#complete").val() +
+  				$("#issue").val() +
+  				$("#all").val() +
+  				$("#task").val()
+  						);
+  });
+});
---
+++ test/test.js
@@ -0,0 +1,24 @@
+var xlsx = require('node-xlsx');
+var fs = require('fs');
+// write
+var obj = {"worksheets":[{"data":[["中文","英文","c"]]}]};
+var file = xlsx.build(obj);
+
+var buffer = xlsx.build({worksheets: [
+  {"name":"第一周", "data":[
+    ["A1", "B1"],
+    [
+      {"value":"A2","formatCode":"General"},
+      {"value":"B2","formatCode":"General"}
+    ]
+  ]},
+  {"name":"第二周", "data":[
+    ["A1", "B1"],
+    [
+      {"value":"A2","formatCode":"General"},
+      {"value":"B2","formatCode":"General"}
+    ]
+  ]}
+]});
+
+fs.writeFileSync('b.xlsx', buffer, 'binary');
