cf8e898ef0d9ead7d900e3ec26ea3240311706f4
Jiang Yikun <yikunkero@gmail.com>
2014-05-04 11:56:57 +0800

    Swtich ejs template.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
